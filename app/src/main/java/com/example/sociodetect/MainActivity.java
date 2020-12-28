4f67011be9a5763feb8bdd98c2e95c45ceca0931
azwaarkhan <azwaar@hotmail.com>
2020-12-29 01:06:44 +0800

    commit

--- app/src/main/java/com/example/sociodetect/MainActivity.java
+++ app/src/main/java/com/example/sociodetect/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< HEAD
         setContentView(R.layout.activity_main_menu);
+=======
+        setContentView(R.layout.activity_main);
+>>>>>>> commit
     }
 }
