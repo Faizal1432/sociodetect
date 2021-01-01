543ca80aec56b05bc234c1241e15579c94672818
azwaarkhan <azwaar@hotmail.com>
2021-01-01 22:42:51 +0800

    commit bro

--- app/src/main/java/com/example/sociodetect/MainActivity.java
+++ app/src/main/java/com/example/sociodetect/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.sociodetect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView cardProfile;
+    CardView cardCamera;
+    CardView cardCOVID;
+    CardView cardInfo;
+    CardView cardRisk;
+    CardView cardFeedback;
+    CardView cardLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardProfile = findViewById(R.id.cardProfile);
+        cardCamera = findViewById(R.id.cardCamera);
+        cardCOVID = findViewById(R.id.cardCOVID);
+        cardInfo = findViewById(R.id.cardInfo);
+        cardRisk = findViewById(R.id.cardRisk);
+        cardFeedback = findViewById(R.id.cardFeedback);
+        cardLogout = findViewById(R.id.cardLogout);
+
+        cardProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, UserFeedback.class);
+                startActivity(intent);
+            }
+        });
     }
 }
