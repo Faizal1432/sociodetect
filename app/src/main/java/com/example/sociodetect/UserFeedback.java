75ebc0136fbe36a7af50efb7810b6126addb578c
azwaarkhan <azwaar@hotmail.com>
2021-01-03 22:56:16 +0800

    commit

--- app/src/main/java/com/example/sociodetect/UserFeedback.java
+++ app/src/main/java/com/example/sociodetect/UserFeedback.java
@@ -1,11 +1,30 @@
 package com.example.sociodetect;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class UserFeedback extends AppCompatActivity {
+
+    EditText text1;
+    EditText text2;
+    EditText text3;
+    Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_feedback);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text1   = (EditText)findViewById(R.id.PersonName);
+                text2   = (EditText)findViewById(R.id.Email);
+                text3   = (EditText)findViewById(R.id.Feedback);
+            }
+        });
     }
 }
